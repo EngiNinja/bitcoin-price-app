ed349d2e54b69fd4c9e8dd583d5f6349fcf024e5
Artem Zamarajev <zamarajev@gmail.com>
2019-06-17 01:54:05 +0200

    implemented value formatter to format xAxis

--- app/src/main/java/com/engininja/bitcoinpriceapp/MyXAxisValueFormatter.java
+++ app/src/main/java/com/engininja/bitcoinpriceapp/MyXAxisValueFormatter.java
@@ -1,16 +1,21 @@
 package com.engininja.bitcoinpriceapp;
 
+public class MyXAxisValueFormatter extends com.github.mikephil.charting.formatter.ValueFormatter {
 
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+    private String[] mValues;
 
-import java.util.Date;
-
-public class MyXAxisValueFormatter implements IAxisValueFormatter {
-
-    @Override
-    public String getFormattedValue(float value, AxisBase axis) {
-        return new Date(new Float(value).longValue()).toString();
+    public MyXAxisValueFormatter(String[] values) {
+        this.mValues = values;
     }
 
+    @Override
+    public String getFormattedValue(float value) {
+        // "value" represents the position of the label on the axis (x or y)
+        // only show every 5-th timestamp
+        if (value%5==0) {
+            return mValues[(int) value];
+        } else {
+            return "";
+        }
+    }
 }
